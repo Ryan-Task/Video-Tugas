22497e23badac635e45f34a85d64872b464f9b5e
Ryan-Task <aryantotrinasrullah77@gmail.com>
2024-10-13 23:26:38 +0700

    video php smk - 15

--- PHP SMK/nav.php
+++ PHP SMK/nav.php
@@ -1,18 +1,15 @@
 <nav>
     <ul>
-        <li>kontak</li>
-        <li>menu</li>
-        <li>jurusan</li>
+        <li><a href="?menu=kontak">kontak</a></li>
+        <li><a href="?menu=sejarah">sejarah</a></li>
+        <li><a href="?menu=jurusan">jurusan</a></li>
     </ul>
 </nav>
 
 <?php  
-if (isset($_POST['kirim'])) {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-    
-        echo $email;
-        echo "<br>";
-        echo $password;
+if (isset($_GET['menu'])) {
+        $menu = $_GET['menu'];
+        
+        require_once $menu.'.php';
     }
 ?>
